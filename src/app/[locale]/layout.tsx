5c32ccd08c2d4cb650645a2bc1211a4c5b5abb7b
Andika113 <kakaa7474@gmail.com>
2026-06-10 09:46:15 +0700

    fix: add Google Tag Manager scripts for analytics tracking in LocaleLayout component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import {setRequestLocale} from 'next-intl/server';
 import {notFound} from 'next/navigation';
+import Script from 'next/script';
 import {routing} from '@/i18n/routing';
 import {AuthProvider} from '@/contexts/AuthContext';
 import {BlogProvider} from '@/contexts/BlogContext';
@@ -33,8 +34,31 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
-      <head />
+      <head>
+        <Script id="google-tag-loader" strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-17860095675" />
+        <Script id="google-tag" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'AW-17860095675');`}
+        </Script>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PKVHSGV9');`}
+        </Script>
+      </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PKVHSGV9"
+            height="0"
+            width="0"
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
         <NextIntlClientProvider messages={messages} locale={locale}>
           <AuthProvider>
             <BlogProvider>
